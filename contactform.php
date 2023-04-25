5ef07c493760ad7dff6cca1a4c0b420558ba2374
Vraj Patel <88333545+Vraj-Pate1@users.noreply.github.com>
2023-04-24 19:33:57 -0500

    4-24-23

--- contactform.php
+++ contactform.php
@@ -3,16 +3,19 @@ $conn = mysqli_connect("localhost", "root", "", "FakeItTillYouMakeIt");
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+
+/* inputs values into table */
 if(isset($_POST['submit'])) {
-  $name = $_POST['name'];
+  $name = $_POST['name']; 
   $email = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
 
-  $sql = "INSERT INTO `contact_form`(`person_id`, `name`, `email`, `subject`, `message`) VALUES (NULL, '$name', '$email', '$subject', '$message')";
+/* connect all data to table */
+$sql = "INSERT INTO `contact_form`(`person_id`, `name`, `email`, `subject`, `message`) VALUES (NULL, '$name', '$email', '$subject', '$message')";
 
-
-  $to = "Vraj9175@outlook.com";
+/* sends the mail */
+$to = "Vraj9175@outlook.com";
   $headers = "From: ".$email;
   $txt = "You have received an email from ".$name.".\n\n".$message;
 
